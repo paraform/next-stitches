54290aa9b0fe4fe115c6fb5ae15ce1dbcbb95dcf
Paraform <code@paraform.studio>
2021-09-16 18:25:11 +0100

    Add stitches

---
+++ stitches.config.ts
@@ -0,0 +1,73 @@
+import { createStitches } from "@stitches/react";
+import type * as Stitches from "@stitches/react";
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  theme: {
+    colors: {
+      gray400: "gainsboro",
+      gray500: "lightgray",
+    },
+  },
+  media: {
+    bp1: "(min-width: 480px)",
+  },
+  utils: {
+    m: (value: Stitches.PropertyValue<"margin">) => ({
+      margin: value,
+    }),
+    mt: (value: Stitches.PropertyValue<"marginTop">) => ({ marginTop: value }),
+    mr: (value: Stitches.PropertyValue<"marginRight">) => ({
+      marginRight: value,
+    }),
+    mb: (value: Stitches.PropertyValue<"marginBottom">) => ({
+      marginBottom: value,
+    }),
+    ml: (value: Stitches.PropertyValue<"marginLeft">) => ({
+      marginLeft: value,
+    }),
+    mx: (value: Stitches.PropertyValue<"margin">) => ({
+      marginLeft: value,
+      marginRight: value,
+    }),
+    my: (value: Stitches.PropertyValue<"margin">) => ({
+      marginTop: value,
+      marginBottom: value,
+    }),
+    p: (value: Stitches.PropertyValue<"padding">) => ({
+      padding: value,
+    }),
+    pt: (value: Stitches.PropertyValue<"paddingTop">) => ({
+      paddingTop: value,
+    }),
+    pr: (value: Stitches.PropertyValue<"paddingRight">) => ({
+      paddingRight: value,
+    }),
+    pb: (value: Stitches.PropertyValue<"paddingBottom">) => ({
+      paddingBottom: value,
+    }),
+    pl: (value: Stitches.PropertyValue<"paddingLeft">) => ({
+      paddingLeft: value,
+    }),
+    px: (value: Stitches.PropertyValue<"padding">) => ({
+      paddingLeft: value,
+      paddingRight: value,
+    }),
+    py: (value: Stitches.PropertyValue<"padding">) => ({
+      paddingTop: value,
+      paddingBottom: value,
+    }),
+  },
+});
+
+export const globalStyles = globalCss({
+  "*": { margin: 0, padding: 0 },
+});
