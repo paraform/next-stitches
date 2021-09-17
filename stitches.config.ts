53a40b59240351dfc548560a0621788305e3da55
Paraform <code@paraform.studio>
2021-09-17 12:46:28 +0100

    Add reset & utils

--- stitches.config.ts
+++ stitches.config.ts
@@ -67,6 +67,28 @@ export const {
       paddingTop: value,
       paddingBottom: value,
     }),
+    size: (value: Stitches.PropertyValue<"width">) => ({
+      width: value,
+      height: value,
+    }),
+    radius: (value: Stitches.PropertyValue<"borderRadius">) => ({
+      borderRadius: value,
+    }),
+
+    shadow: (value: Stitches.PropertyValue<"boxShadow">) => ({
+      boxShadow: value,
+    }),
+
+    linearGradient: (value: Stitches.PropertyValue<"backgroundImage">) => ({
+      backgroundImage: `linear-gradient(${value})`,
+    }),
+
+    // Component
+    stackGap:
+      <T extends { theme: { space: any } }>(_: T) =>
+      (value: `$${Extract<keyof T["theme"]["space"], string | number>}`) => ({
+        $$gap: `$space${value}`,
+      }),
   },
 });
 
@@ -88,7 +110,139 @@ export const darkTheme = createTheme("dark-theme", {
 
 export const themes = [lightTheme, darkTheme];
 
-export const globalStyles = globalCss({
+type CSS = Stitches.CSS<typeof config>;
+
+export const reset: Record<string, CSS> = {
+  html: {
+    lineHeight: 1.15,
+    "-webkit-text-size-adjust": "100%",
+  },
+  body: {
+    margin: 0,
+  },
+  main: {
+    display: "block",
+  },
+  h1: {
+    fontSize: "2em",
+    margin: "0.67em 0",
+  },
+  hr: {
+    boxSizing: "content-box",
+    height: 0,
+    overflow: "visible",
+  },
+  pre: {
+    fontFamily: "monospace, monospace",
+    fontSize: "1em",
+  },
+  a: {
+    backgroundColor: "transparent",
+  },
+  "abbr[title]": {
+    borderBottom: "none",
+    textDecoration: "underline dotted",
+  },
+  "b, strong": {
+    fontWeight: "bolder",
+  },
+  "code, kbd, samp": {
+    fontFamily: "monospace, monospace",
+    fontSize: "1em",
+  },
+  small: {
+    fontSize: "80%",
+  },
+  "sub, sup": {
+    fontSize: "75%",
+    lineHeight: 0,
+    position: "relative",
+    verticalAlign: "baseline",
+  },
+  sub: {
+    bottom: "-0.25em",
+  },
+  sup: {
+    top: "-0.5em",
+  },
+  img: {
+    borderStyle: "none",
+  },
+  "button, input, optgroup, select, textarea": {
+    fontFamily: "inherit",
+    fontSize: "100%",
+    lineHeight: 1.15,
+    margin: 0,
+  },
+  "button, input": {
+    overflow: "visible",
+  },
+  "button, select": {
+    textTransform: "none",
+  },
+  'button, [type="button"], [type="reset"], [type="submit"]': {
+    "-webkit-appearance": "button",
+  },
+  'button::-moz-focus-inner, [type="button"]::-moz-focus-inner, [type="reset"]::-moz-focus-inner, [type="submit"]::-moz-focus-inner':
+    {
+      borderStyle: "none",
+      padding: 0,
+    },
+  'button:-moz-focusring, [type="button"]:-moz-focusring, [type="reset"]:-moz-focusring, [type="submit"]:-moz-focusring':
+    {
+      outline: "1px dotted ButtonText",
+    },
+  fieldset: {
+    padding: "0.35em 0.75em 0.625em",
+  },
+  legend: {
+    boxSizing: "border-box",
+    color: "inherit",
+    display: "table",
+    maxWidth: "100%",
+    padding: 0,
+    whiteSpace: "normal",
+  },
+  progress: {
+    verticalAlign: "baseline",
+  },
+  textarea: {
+    overflow: "auto",
+  },
+  '[type="checkbox"], [type="radio"]': {
+    boxSizing: "border-box",
+    padding: 0,
+  },
+  '[type="number"]::-webkit-inner-spin-button, [type="number"]::-webkit-outer-spin-button':
+    {
+      height: "auto",
+    },
+  '[type="search"]': {
+    "-webkit-appearance": "textfield",
+    outlineOffset: -2,
+  },
+  '[type="search"]::-webkit-search-decoration': {
+    "-webkit-appearance": "none",
+  },
+  "::-webkit-file-upload-button": {
+    "-webkit-appearance": "button",
+    font: "inherit",
+  },
+  details: {
+    display: "block",
+  },
+  summary: {
+    display: "list-item",
+  },
+  template: {
+    display: "none",
+  },
+  "[hidden]": {
+    display: "none",
+  },
+};
+
+export const globalStyles = globalCss(reset, {
   "*": { margin: 0, padding: 0 },
 
   "body,[class*='-theme']": {
